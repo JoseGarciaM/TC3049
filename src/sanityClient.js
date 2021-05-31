d3b4c04f444b3bd4d29c69a03c2baedbbf871560
Jose Carlos Garcia Mendizabal <info@josegarciam.com>
2021-05-31 16:39:36 -0500

    small changes

--- src/sanityClient.js
+++ src/sanityClient.js
@@ -1,10 +1,4 @@
 const sanityClient = require('@sanity/client');
 const {sanityClientConfig} = require('../sanityClientConfig');
 
-if (sanityClientConfig.projectId === 'bl468k7z') {
-  console.error(
-      'Please change the projectId in ./sanityClientConfig.js to match the projectId found in ./studio/sanity.json',
-  );
-}
-
 export default sanityClient(sanityClientConfig);
